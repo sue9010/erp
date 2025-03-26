9cddacad0ed1604a2225c14f0d787163ba4a87fb
soo8oct <min.ress@gmail.com>
2025-03-26 09:04:08 +0900

    product_part 변경 취소

--- frontend/src/pages/ProductPage.js
+++ frontend/src/pages/ProductPage.js
@@ -4,22 +4,15 @@ import { API_BASE } from "../api/config";
 
 function ProductPage() {
   const [products, setProducts] = useState([]);
-  const [modules, setModules] = useState([]);
-  const [lenses, setLenses] = useState([]);
-  const [sensors, setSensors] = useState([]);
 
   const [newProduct, setNewProduct] = useState({
     name: "",
     manufacturer: "",
     price: "",
-    module: "",
-    lens: "",
-    sensor: ""
   });
 
   useEffect(() => {
     fetchProducts();
-    fetchParts();
   }, []);
 
   const fetchProducts = () => {
@@ -28,16 +21,6 @@ function ProductPage() {
       .catch((err) => console.error("불러오기 실패:", err));
   };
 
-  const fetchParts = () => {
-    axios.get(`${API_BASE}/product-parts`)
-      .then((res) => {
-        const all = res.data;
-        setModules(all.filter(p => p.type === "모듈"));
-        setLenses(all.filter(p => p.type === "렌즈"));
-        setSensors(all.filter(p => p.type === "센서"));
-      })
-      .catch((err) => console.error("부품 불러오기 실패:", err));
-  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -51,10 +34,7 @@ function ProductPage() {
           name: "",
           manufacturer: "",
           price: "",
-          module: "",
-          lens: "",
-          sensor: ""
-        });
+       });
         fetchProducts();
       })
       .catch((err) => console.error("등록 실패:", err));
@@ -67,8 +47,6 @@ function ProductPage() {
         {products.map((p) => (
           <li key={p.id}>
             {p.name} / {p.manufacturer} / {p.price?.toLocaleString()}원 / 재고: {p.stock ?? 0}개
-            <br />
-            구성: {p.module} / {p.lens} / {p.sensor}
           </li>        
         ))}
       </ul>
@@ -96,29 +74,6 @@ function ProductPage() {
           onChange={(e) => setNewProduct({ ...newProduct, price: e.target.value })}
           required
         />
-
-        <h3>🧩 구성 선택</h3>
-        <select value={newProduct.module} onChange={(e) => setNewProduct({ ...newProduct, module: e.target.value })} required>
-          <option value="">-- 모듈 선택 --</option>
-          {modules.map((m) => (
-            <option key={m.id} value={m.name}>{m.name}</option>
-          ))}
-        </select>
-
-        <select value={newProduct.lens} onChange={(e) => setNewProduct({ ...newProduct, lens: e.target.value })} required>
-          <option value="">-- 렌즈 선택 --</option>
-          {lenses.map((l) => (
-            <option key={l.id} value={l.name}>{l.name}</option>
-          ))}
-        </select>
-
-        <select value={newProduct.sensor} onChange={(e) => setNewProduct({ ...newProduct, sensor: e.target.value })} required>
-          <option value="">-- 센서 선택 --</option>
-          {sensors.map((s) => (
-            <option key={s.id} value={s.name}>{s.name}</option>
-          ))}
-        </select>
-
         <br /><br />
         <button type="submit">등록</button>
       </form>
